ca45e55dd25942f769a2ceb570fa216d18f54df6
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-02 14:56:53 -0500

    Update DND2024CharacterCreation.js

--- DataValidation/DND2024CharacterCreation.js
+++ DataValidation/DND2024CharacterCreation.js
@@ -1,4 +1,4 @@
 var classSelects = document.GetElementByClassName("CharacterClass");
 var classOptions = ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
 
-selects.options.add(new Option(classOptions));
+selects.options = classOptions;
