cb0f2a16e5066fd07926b6cd392928f322b6d627
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-04 12:13:38 -0500

    Update DND2024CharacterCreation.js

--- DataValidation/DND2024CharacterCreation.js
+++ DataValidation/DND2024CharacterCreation.js
@@ -1,14 +1,3 @@
-var classSelects = document.GetElementByClassName("CharacterClass");
-
-function myDisplayer(some) {
-  document.getElementById("demo").innerHTML = some;
-}
-
-async function AssignClassOptions() {return "hello";}// ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];}
-
-AssignClassOptions().then(
-  function(value) {myDisplayer(value);},
-  function(error) {myDisplayer(error);}
-);
+document.getElementById("demo").innerHTML = "Hello";// ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];}
 
 
