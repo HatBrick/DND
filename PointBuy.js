ab08f5f4f6050fe19ab76ea9da53970d3a8a31a9
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-05 16:37:59 -0500

    Update PointBuy.js

--- PointBuy.js
+++ PointBuy.js
@@ -1,18 +1,27 @@
 var pointTotal = 75;
 var points = document.getElementById("PointBuy");
 var stats = document.getElementsByClassName("stat");
+var incButtons = document.getElementsByClassName("inc");
+var decButtons = document.getElementsByClassName("dec");
 var statValTotal = 0;
 
-for(let i = 0; i < stats.length; i++) {
-  stats[i].addEventListener("input", UpdateStatValue);
+for(let i = 0; i < incButtons.length; i++) {
+  incButtons[i].addEventListener("input", UpdateStatValue);
 }
 
-function UpdateStatValue(e) {
+function UpdateStatValue() {
+  thisStat = document.getElementByID(this.id);
+  if(this.class == "Inc") {
+    thisStat.value = parseInt(thisStat.value) + 1;
+  } else {
+    thisStat.value = parseInt(thisStat.value) - 1;
+  }
+  
   statValTotal = 0;
   for(let i = 0; i < stats.length; i++) {
     statValTotal += parseInt(stats[i].value);
   }
-  UpdatePointBuy(e);
+  UpdatePointBuy(thisStat.value);
 }
 
 function UpdatePointBuy(e) {
