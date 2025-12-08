6e44cf3b63913b4079d678ed4752c26eaec072ee
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-08 16:04:28 -0500

    Create CC_DND_ClassManager.js

---
+++ CC_DND_ClassManager.js
@@ -0,0 +1,41 @@
+//On Page Load
+const charClassSelect = document.getElementById("ClassSelect");
+const charLevelSelect = document.getElementById("LevelSelect");
+const classDesc = document.getElementById("ClassDescription");
+document.getElementById("ClassSelect").onload = function() {SetSelects()};
+
+const fileInput = "test.text";
+const reader = new FileReader();
+
+reader.addEventListener("load", () => {
+  // this will then display a text file
+  content.innerText = reader.result;
+});
+
+if (fileInput) {
+  reader.readAsText(fileInput);
+}
+
+//Set Selects
+function SetSelects() {
+  //Set Class Selects
+  var classOptions = ["--Select Your Class--", "Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
+  for (let i=0; i < classOptions.length; i++) {
+    let option = document.createElement("option");
+    option.setAttribute("value", classOptions[i]);
+    option.text = classOptions[i];
+    charClassSelect.appendChild(option);
+  }
+  //Set Level Selects
+  var levelOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  for (let i=0; i < levelOptions.length; i++) {
+    let option = document.createElement("option");
+    option.setAttribute("value", levelOptions[i]);
+    option.text = levelOptions[i];
+    charLevelSelect.appendChild(option);
+  }
+}
+
+function UpdateDescription() {
+  
+}
