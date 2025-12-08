0ff70e75f349f61fddca0eca98a6019f33bfa901
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-08 11:20:31 -0500

    Update PointBuy.js

--- PointBuy.js
+++ PointBuy.js
@@ -6,15 +6,14 @@ var decButtons = document.getElementsByClassName("Dec");
 var statValTotal = 0;
 
 for(let i = 0; i < incButtons.length; i++) {
-  console.log(incButtons[i].id);
-  console.log(decButtons[i].id);
   incButtons[i].addEventListener("click", UpdateStatValue);
   decButtons[i].addEventListener("click", UpdateStatValue);
+  decButtons[i].disabled = true;
 }
 
 function UpdateStatValue() {
   thisStat = document.getElementById(this.id);
-  if(this.class == "Inc") {
+  if(this.className == "Inc") {
     thisStat.innerHTML = parseInt(thisStat.innerHTML) + 1;
   } else {
     thisStat.innerHTML = parseInt(thisStat.innerHTML) - 1;
@@ -32,4 +31,18 @@ function UpdatePointBuy(e) {
   if(e == 14) {pointsRemain - 1;}
   if(e == 15) {pointsRemain - 2;}
   points.value = pointsRemain;
+  EnableDisable(pointsRemain);
+}
+
+function EnableDisable(pointsRemain) {
+  for(let i = 0; i < incButtons.length; i++) {
+    if(pointsRemain == 0) {
+      incButtons[i].disabled = true;
+    } else if (pointsRemain == 27) {
+      decButtons[i].disabled = true;
+    } else {
+      incButtons[i].disabled = false;
+      decButtons[i].disabled = false;
+    }
+  }
 }
