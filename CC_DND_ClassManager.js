903cc2c1aadcb44935c0a177fc00dbe8d923f03b
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-10 14:23:40 -0500

    Update CC_DND_ClassManager.js

--- CC_DND_ClassManager.js
+++ CC_DND_ClassManager.js
@@ -2,7 +2,6 @@
 const charClassSelect = document.getElementById("ClassSelect");
 const charLevelSelect = document.getElementById("LevelSelect");
 const classDesc = document.getElementById("ClassDescription");
-var formsList = new Array();
 
 LoadForms("./CharacterCreationResources/DND2024Creation.txt");
 
@@ -11,37 +10,34 @@ function LoadForms(formRef) {
 fetch('./CharacterCreationResources/DND2024Creation.txt') 
   .then(response => { return response.text(); })
   .then(textData => { 
-    formsList = textData.split("|");
-    SetSelects(); })
+    SplitForms(textData.split("|"));
+  })
   .catch(error => {
     // Handle any errors that occurred during the fetch operation
     console.error('Error fetching the text file:', error);
   });
 }
 
-//Set Selects
-function SetSelects() {
-  console.log(formsList.length);
-  console.log(formsList[0]);
-  console.log(formsList[1]);
-  //Set Class Selects
-  var classOptions = ["--Select Your Class--", "Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
-  for (let i=0; i < classOptions.length; i++) {
-    let option = document.createElement("option");
-    option.setAttribute("value", classOptions[i]);
-    option.text = classOptions[i];
-    charClassSelect.appendChild(option);
-  }
-  //Set Level Selects
-  var levelOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-  for (let i=0; i < levelOptions.length; i++) {
-    let option = document.createElement("option");
-    option.setAttribute("value", levelOptions[i]);
-    option.text = levelOptions[i];
-    charLevelSelect.appendChild(option);
+function SplitForms(formsList) {
+  for(let i = 0; i < formsList.length; i++) {
+    CreateCharacterForms(formsList[i].split(", "));
   }
 }
 
-function UpdateDescription() {
-  
+function CreateCharacterForms(arr) {
+  if(arr[0] == "Select") { CreateSelect(arr); }
+  const charClassSelect = document.getElementById("ClassSelect");
+}
+
+function CreateSelect(arr) {
+  let select = document.createElement("select");
+  select.className = "CharacterForm";
+  select.id = arr[1];
+  for (let i = 2; i < arr.length; i++) {
+    let option = document.createElement("option");
+    option.setAttribute("value", arr[i]);
+    option.text = arr[i];
+    select.appendChild(arr);
+  }
+  document.body.appendChild(select);
 }
