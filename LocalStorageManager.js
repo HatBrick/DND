653f74fe1b124a93137d007b014a01117d42c75c
HatBrick <73493101+HatBrick@users.noreply.github.com>
2025-12-16 11:44:28 -0500

    Update LocalStorageManager.js

--- LocalStorageManager.js
+++ LocalStorageManager.js
@@ -1,14 +1,11 @@
-function CreateLocalSave() {
-  const pClass = document.querySelector("select#Class");
-  const pLevel = document.querySelector("select#Level");
-  const pSpecies = document.querySelector("select#Species");
-  StoreLocalSave();
-}
+const pClass = document.querySelector("select#Class");
+const pLevel = document.querySelector("select#Level");
+const pSpecies = document.querySelector("select#Species");
 
 function StoreLocalSave() {
-  localStorage.setItem("Class", pClass).value;
-  localStorage.setItem("Level", pLevel).value;
-  localStorage.setItem("Species", pSpecies).value;
+  localStorage.setItem("Class", pClass.value);
+  localStorage.setItem("Level", pLevel.value);
+  localStorage.setItem("Species", pSpecies.value);
 }
 
 function RetrieveLocalSave() {
